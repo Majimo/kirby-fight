3f59f04b05c077bcc516a07688c993ffec9950cf
majimo <pierre.fervel@gmail.com>
2018-07-13 15:45:56 +0200

    Introduced LifePoints

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -10,11 +10,15 @@ export class MainComponent {
   randomNumber: number;
   randomKey: string;
   userValue: string;
+  playerLife: number;
+  ennemyLife: number;
 
   constructor() {
   }
 
   giveRandomKey(userKey) {
+    this.playerLife = parseInt(document.getElementById("life-player1").innerHTML);
+    this.ennemyLife = parseInt(document.getElementById("life-player2").innerHTML);
     MainComponent.start = Date.now();
     userKey.value = null;
     this.randomNumber = Math.floor(Math.random() * 26) + 65;
@@ -26,11 +30,13 @@ export class MainComponent {
     var millis = Math.floor((Date.now() - MainComponent.start) / 100);
     this.userValue = event.target.value;
     if (this.userValue.toUpperCase() === this.randomKey && millis <= 20) {
+      document.getElementById("life-player2").innerHTML = (this.ennemyLife - 1).toString();
       console.log("Secondes écoulées : " + millis / 10);
       console.log("Vous avez pressé la lettre : " + this.userValue.toUpperCase());
       console.log("Gagné !");
     }
     else {
+      document.getElementById("life-player1").innerHTML = (this.playerLife - 1).toString();
       console.log("Secondes écoulées : " + millis / 10);
       console.log("Vous avez pressé la lettre : " + this.userValue.toUpperCase());
       console.log("Perdu !");
