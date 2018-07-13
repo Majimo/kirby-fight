b9ffe121191322073a4830806e86fd39e08249a0
majimo <pierre.fervel@gmail.com>
2018-07-13 12:07:28 +0200

    Including timer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  static start: number;
   randomNumber: number;
   randomKey: string;
   userValue: string;
@@ -14,18 +15,27 @@ export class MainComponent {
   }
 
   giveRandomKey(userKey) {
+    MainComponent.start = Date.now();
     userKey.value = null;
     this.randomNumber = Math.floor(Math.random() * 26) + 65;
     this.randomKey = String.fromCharCode(this.randomNumber);
     userKey.focus();
   }
 
-  getUserKey(event: any) { 
+  getUserKey(event: any) {
+    var millis = Math.floor((Date.now() - MainComponent.start) / 100);
     this.userValue = event.target.value;
     console.log(this.userValue.toUpperCase());
     console.log(this.randomKey);
-    if (this.userValue.toUpperCase() === this.randomKey) {
+    if (this.userValue.toUpperCase() === this.randomKey && millis <= 20) {
+      console.log("Secondes écoulées : " + millis / 10);
+      console.log("Vous avez pressé la lettre : " + this.userValue.toUpperCase());
       console.log("Gagné !");
+    }
+    else {
+      console.log("Secondes écoulées : " + millis / 10);
+      console.log("Vous avez pressé la lettre : " + this.userValue.toUpperCase());
+      console.log("Perdu !");
     }
   }
 
