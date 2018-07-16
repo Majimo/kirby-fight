9c6429cb6f645b57db91ebf315c47f6455e076b8
majimo <pierre.fervel@gmail.com>
2018-07-16 12:38:58 +0200

    Added Bootstrap 4

--- src/app/components/fight-result/fight-result.component.ts
+++ src/app/components/fight-result/fight-result.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'fight-result',
@@ -6,20 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./fight-result.component.scss']
 })
 export class FightResultComponent {
-
+ 
   fightResult;
 
   constructor() {
-    this.fightResult = document.getElementById("fight-result");
   }
 
   playerWin(millis, userValue) {
+    this.fightResult = document.getElementById("fightResult");
     this.fightResult.innerHTML = "Secondes écoulées : " + millis / 10 + "<br>";
     this.fightResult.innerHTML += "Vous avez pressé la lettre : " + userValue.toUpperCase() + "<br>";
     this.fightResult.innerHTML += "L'ennemi perd un point de vie !";
   }
 
   ennemyWin(millis, userValue) {
+    this.fightResult = document.getElementById("fightResult");
     this.fightResult.innerHTML = "Secondes écoulées : " + millis / 10 + "<br>";
     this.fightResult.innerHTML += "Vous avez pressé la lettre : " + userValue.toUpperCase() + "<br>";
     this.fightResult.innerHTML += "Vous perdez un point de vie !";
